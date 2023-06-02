fd6b6b6a0c7989524182e6f6147bce63538b24cc
jjangyeunii <euneun0927@naver.com>
2023-06-02 15:47:48 +0900

    profile - hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
-export default function Home() {
-  return <section className="bg-gray-500">main</section>;
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
