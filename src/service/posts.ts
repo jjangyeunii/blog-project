acdd1d839b96d587d650b16ba015c8816a673d89
jjangyeunii <euneun0927@naver.com>
2023-06-16 13:32:28 +0900

    blog post - page

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,3 +1,4 @@
+import { metadata } from "./../app/layout";
 import { readFile } from "fs/promises";
 import path from "path";
 
@@ -9,6 +10,8 @@ export type Post = {
   path: string;
   featured: boolean;
 };
+
+export type PostData = Post & { content: string };
 
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts() //
@@ -25,4 +28,15 @@ export async function getAllPosts(): Promise<Post[]> {
   return readFile(filePath, "utf-8")
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const metadata = await getAllPosts() //
+    .then((posts) => posts.find((post) => post.path === fileName));
+  if (!metadata)
+    throw new Error(`${fileName}에 해당파는 포스트를 찾을 수 없음`);
+
+  const content = await readFile(filePath, "utf-8");
+  return { ...metadata, content };
 }
