a767a00c2bb4b260a985c50a41a0289ee48fa738
jjangyeunii <euneun0927@naver.com>
2023-06-24 23:38:30 +0900

    about - page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,35 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
+
 export default function AboutPage() {
-  return <p>나에 대해!</p>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          주어진 환경에서 성장하는 프론트엔드 개발자
+          <br />
+          사용자를 위한 웹을 만들고 싶은 신입 개발자입니다
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          성신여자대학교 의류산업학과 전공
+          <br />
+          IT비즈니스공학 복수전공(자기설계전공)
+          <br />
+          코드스테이츠 부트캠프 수료
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          Javascript, Typescript, HTML, CSS
+          <br />
+          React, Styled-components, TailwindCSS
+          <br />
+          Git, VS Code
+        </p>
+      </section>
+    </>
+  );
 }
