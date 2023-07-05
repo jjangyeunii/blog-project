9d6442c08e343eebd64e55b48713f806b3320a8c
jjangyeunii <euneun0927@naver.com>
2023-07-05 14:11:22 +0900

    성능점검 및 개선

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -18,7 +18,6 @@ export default function ContactPage() {
   return (
     <section className="flex flex-col items-center">
       <h2 className="text-3xl font-bold my-2">Contact Me</h2>
-      <p>jangyeun0927@naver.com</p>
       <ul className="flex gap-6 my-2">
         {LINKS.map((link, index) => (
           <a
